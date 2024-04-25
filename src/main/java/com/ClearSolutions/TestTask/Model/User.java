36d281290622920e511ee32edbc3ab0d3343b805
Kostia <k512110d@gmail.com>
2024-04-25 13:25:02 +0300

    added 'User' model and dto

---
+++ src/main/java/com/ClearSolutions/TestTask/Model/User.java
@@ -0,0 +1,53 @@
+package com.ClearSolutions.TestTask.Model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Pattern;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@ToString
+@Table(name = "users")
+public class User {
+
+    @Id
+    private Long id;
+
+    @Email(message = "Must be a valid e-mail address")
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Pattern(regexp = "[A-Z][a-z]+",
+            message = "Must start with a capital letter followed by one or more lowercase letters")
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Pattern(regexp = "[A-Z][a-z]+",
+            message = "Must start with a capital letter followed by one or more lowercase letters")
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Past(message = "Must be earlier that current date")
+    @Column(nullable = false)
+    private LocalDate birthDate;
+
+    @NotBlank(message = "The 'address' cannot be blank")
+    private String address;
+
+    @Pattern(regexp = "^(\\d{10}|(?:\\d{3}[- ]){2}\\d{4}|\\(\\d{3}\\)\\d{3}-?\\d{4})$")
+    private String phoneNumber;
+
+}
